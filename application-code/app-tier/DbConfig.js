b0e241c3eca49727f0d9e067e48322f06e2c6e24
avizway1 <57447641+avizway1@users.noreply.github.com>
2024-07-12 13:07:20 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'mydb.cfpgnjehw330.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Avinash12345',
+    DB_DATABASE : 'webappdb'
+});
