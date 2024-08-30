23c2c6600b30f4531d0237b880c0ab2a5ef095cb
pharmetradegithub <pharmetrade630@gmail.com>
2024-08-30 12:52:29 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'my-hds-project.cxw4i6sswnh3.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'YourPassword',
+    DB_PWD : 'chakrirandhi',
     DB_DATABASE : 'webappdb'
 });
